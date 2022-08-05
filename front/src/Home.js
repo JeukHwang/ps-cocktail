8996dcf657fed3671f664befd7931529e8801c8d
JeukHwang <92910273+JeukHwang@users.noreply.github.com>
2022-08-06 06:38:36 +0900

    feat: home

--- front/src/Home.js
+++ front/src/Home.js
@@ -1,60 +1,7 @@
-import { useState } from "react";
 import "./Home.css";
-import { searchIngredient } from "./localbackend/index.js";
-
-function UserDataRow({ title, maxLength, setter }) {
-    return (
-        <div className="user-data-row">
-            <div className="user-data-row-title">{title}</div>
-            <input
-                className="user-data-row-input"
-                id={"user-data-row-input-" + title}
-                maxLength={maxLength}
-                onChange={(e) => {
-                    setter(e.target.value);
-                }}
-            />
-        </div>
-    );
-}
-function Button({ title, id, setUserDt, userDt }) {
-    return (
-        <button
-            id="home-button"
-            onClick={() => {
-                if (id === "a") {
-                    console.log(id);
-                    var dad = searchIngredient("");
-                    console.log(dad);
-                }
-            }}
-        >
-            {title}
-        </button>
-    );
-}
-
-function InputUserData({ title, setId }) {
-    return (
-        <div className="input-user-data">
-            <UserDataRow title={title} shown={true} setter={setId} />
-        </div>
-    );
-}
-
-function Showlist({ title, props }) {
-    return <div className="image-list"></div>;
-}
-
-function Showind({ }) {
-    const [isOpen, setIsOpen] = useState(false);
-    const openSubnavbar = () => setIsOpen(true);
-    const closeSubnavbar = () => setIsOpen(false);
-    return <div id="showbar" style={isOpen ? {} : { height: 0 }}></div>;
-}
+import { autoComplete } from "./localbackend/index.js";
 
 function Home(props) {
-    const [id, setId] = useState("");
     return (
         <div className="home">
             <div className="logo">🍸</div>
@@ -64,64 +11,88 @@ function Home(props) {
                 </div>
                 <div id="autocomplete" className="autocomplete"></div>
             </div>
-            <InputUserData title={"Ingredient"} setId={setId} />
-            <Button
-                title={"add"}
-                id={id}
-                setUserDt={props.setUserDt}
-                userDt={props.userDt}
-            />
-            <Showind />
-            <div className="home-info">
-                <InputUserData title={"list"} setId={setId} />
-                <Button
-                    title={"search"}
-                    id={id}
-                    setUserDt={props.setUserDt}
-                    userDt={props.userDt}
-                />
-            </div>
-            <Showlist title={"cocktail"} setId={setId} />
+            <div id="ingredients" className="ingredients"></div>
         </div>
     );
 }
 
 window.onload = () => {
     const searchInput = document.getElementById("searchInput");
-    function addIntegredient() {
-        if (searchInput.value === "") { return; }
-        console.log("add!");
-        searchInput.value = "";
-        return;
-    }
     document.body.addEventListener("keypress", (event) => {
-        if (event.key === "Enter") { addIntegredient(); }
-    });
-    const autocomplete = document.getElementById("autocomplete");
-    function setAutocomplete(words) {
-        while (autocomplete.lastElementChild) {
-            autocomplete.removeChild(autocomplete.lastElementChild);
+        if (event.key === "Enter") {
+            if (searchInput.value === "") { return; }
+            console.log("add!");
+            addIngredient(searchInput.value);
+            searchInput.value = "";
+            return;
         }
-        words.forEach((word) => {
+    });
+    const autocompleteElem = document.getElementById("autocomplete");
+    function setAutocomplete() {
+        while (autocompleteElem.lastElementChild) {
+            autocompleteElem.removeChild(autocompleteElem.lastElementChild);
+        }
+        if (searchInput.value.length < 2) { return; }
+        const words = autoComplete(searchInput.value);
+        const maxNum = 5;
+        [...Array(maxNum).keys()].forEach((i) => {
+            if (!words[i]) { return; }
+            const [type, word] = words[i];
             const text = document.createElement("div");
             text.classList.add("autocomplete-words");
-            text.innerText = word;
+            text.innerText = `${type} ${word}`;
             text.addEventListener("click", () => {
                 searchInput.value = text.innerText;
-                const data = searchInput.value !== "" ? [searchInput.value, searchInput.value + "1", searchInput.value + "2"] : [];
-                setAutocomplete(data);
+                setAutocomplete();
             });
-            autocomplete.appendChild(text);
+            autocompleteElem.appendChild(text);
         });
+        if (words.length -maxNum > 0) {
+            const text = document.createElement("div");
+            text.classList.add("autocomplete-words");
+            text.innerText = `${words.length - maxNum} more results ...`;
+            autocompleteElem.appendChild(text);
+        }
     }
     let debounce;
     searchInput.addEventListener("input", () => {
         clearTimeout(debounce);
-        debounce = setTimeout(() => {
-            const data = searchInput.value !== "" ? [searchInput.value, searchInput.value + "1", searchInput.value + "2"] : [];
-            setAutocomplete(data);
-        }, 300);
+        debounce = setTimeout(() => { setAutocomplete(); }, 100);
     });
+    const ings = document.getElementById("ingredients");
+
+    function removeIngredient(name) {
+        const localIngs = localStorage.getItem("ings");
+        if (localIngs.includes(name)) {
+            const index = localIngs.indexOf(name);
+            if (index > -1) { localIngs.splice(index, 1); }
+        }
+        localStorage.setItem("ings", localIngs);
+        const element = document.getElementById(`ing-${name}`);
+        element.remove();
+    }
+
+    function addIngredient(name) {
+        localStorage.setItem("ings", [...localStorage.getItem("ings"), name]);
+        const ing = document.createElement("div");
+        ing.id = `ing-${name}`;
+        ing.classList.add("ingredient");
+        ing.innerText = name;
+        ing.addEventListener("click", () => {
+            window.location.href = `/cocktail/${name}`;
+        });
+        ing.addEventListener('contextmenu', (event) => {
+            event.preventDefault();
+            removeIngredient(name);
+        });
+        ings.appendChild(ing);
+    }
+    const localIngs = localStorage.getItem("ings");
+    if (!localIngs) {
+        localStorage.setItem("ings", []); // Initial
+    } else {
+        localStorage.getItem("ings").forEach((ing) => { addIngredient(ing); });
+    }
 };
 
 export default Home;
