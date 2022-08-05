a32f505f2fc79ce88319df9a9fcafacb8652fb7e
sangoh <sangohyeh@gmail.com>
2022-08-06 01:34:30 +0900

    bug: 임시저장

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,31 @@
 import cors from "cors";
 // import "dotenv/config";
 import express from "express";
+import mysql from "mysql";
+
+const connection = mysql.createConnection({
+  host     : '34.64.69.222',
+  user     : 'root',
+  password : '0701',
+  database : 'cocktail'
+});
+
+connection.connect();
 
 const app: express.Application = express();
 app.use(cors());
 
 app.get('/', (req:express.Request, res:express.Response) => {
   res.send('Hello World!')
+})
+
+
+app.post("/batch/database/setting", (req:express.Request, res:express.Response) => {
+  
+  connection.query()
+
+  
+  res.send('database setting finish')
 })
 
 const port = 3000;
